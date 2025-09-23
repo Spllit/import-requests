8130b186e818696aeb80a1fa2816facdb47ef1fe
Spllit <0flacks6@gmail,com>
2025-09-23 17:28:39 +0300

    исправлено расположение кнопки

--- src/getCustomField.js
+++ src/getCustomField.js
@@ -1,11 +1,5 @@
 export default function getCustomField({formId, customFieldId}){
     return(
-        // `
-        // <div class="mb-3 relative" name="customField" data-custom-field-id="${customFieldId}", data-customField-formId="${formId}">
-        //     <input type="text" name="customFieldApi" placeholder="название поля в API" class=" w-50 mr-3 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200">
-        //     <input type="text" name="customFieldValue" placeholder="значение" class=" w-80 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition duration-200">
-        // </div>
-        // `
         ` 
 <div class="mb-3 flex flex-wrap lg:flex-nowrap w-full gap-3" name="customField" data-custom-field-id="${customFieldId}" data-customfield-formid="${formId}">
   <div class="w-full w-1/3 md:w-full min-w-[100px]">
--- src/getForm.js
+++ src/getForm.js
@@ -36,7 +36,7 @@ export default function getForm(id){
                                         <div class="flex justify-between ">
                                             <div class="flex flex-col flex-grow space-y-2 relative">
                                                 <span>Название заявки</span>
-                                                <button tabindex="-1" data-duplicate="subject" name="duplicateInputData" class="absolute top-10 right-0">
+                                                <button tabindex="-1" data-duplicate="subject" name="duplicateInputData" class="absolute top-8 right-0">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="none" stroke="#D1D5DC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 64 64">
                                                     <rect x="12" y="12" width="32" height="32" rx="8"/>
                                                     <rect x="20" y="20" width="32" height="32" rx="8"/>
